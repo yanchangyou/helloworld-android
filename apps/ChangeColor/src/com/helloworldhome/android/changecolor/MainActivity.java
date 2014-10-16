c3bad0584400ee0e0919fbe92a80e3454c713468
yanchangyou <yanchangyou@gmail.com>
2014-10-16 14:59:56 +0800

    重构代码 1， 项目名称规范 2， 项目包规范 3， 使用android-4.0版本，去掉依赖项目 4， 界面显示优化：对齐，margin，颜色

--- apps/ChangeColor/src/com/helloworldhome/android/changecolor/MainActivity.java
+++ apps/ChangeColor/src/com/helloworldhome/android/changecolor/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.change_color;
+package com.helloworldhome.android.changecolor;
 
+
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,37 +11,36 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final TextView text = (TextView) findViewById(R.id.hello);
-		Button redBtn = (Button) findViewById(R.id.btn_red);
-		Button greenBtn = (Button) findViewById(R.id.btn_green);
-		Button blueBtn = (Button) findViewById(R.id.btn_blue);
-		redBtn.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				text.setBackgroundColor(Color.RED);
-			}
-		});
-		greenBtn.setOnClickListener(new OnClickListener() {
+		final TextView text = (TextView) findViewById(R.id.text);
 
-			@Override
-			public void onClick(View v) {
-				text.setBackgroundColor(Color.GREEN);
-			}
-		});
-		blueBtn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				text.setBackgroundColor(Color.BLUE);
-			}
-		});
+		((Button) findViewById(R.id.button_red))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						text.setBackgroundColor(Color.RED);
+					}
+				});
+		((Button) findViewById(R.id.button_green))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						text.setBackgroundColor(Color.GREEN);
+					}
+				});
+		((Button) findViewById(R.id.button_blue))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						text.setBackgroundColor(Color.BLUE);
+					}
+				});
 	}
 
 	@Override
@@ -55,15 +55,18 @@ public class MainActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		final TextView text = (TextView) findViewById(R.id.hello);
+
+		final TextView text = (TextView) findViewById(R.id.text);
+
 		int id = item.getItemId();
 		if (id == R.id.action_red) {
 			text.setBackgroundColor(Color.RED);
-		} else if (id ==R.id.action_green) {
+		} else if (id == R.id.action_green) {
 			text.setBackgroundColor(Color.GREEN);
-		} else if (id ==R.id.action_blue) {
+		} else if (id == R.id.action_blue) {
 			text.setBackgroundColor(Color.BLUE);
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 }
