685720aa01bbe80f54ce3799c051026dc01d8538
yanchangyou <yanchangyou@gmail.com>
2014-10-17 10:03:01 +0800

    去掉菜单：目前不需要

--- apps/TouchMe/src/com/helloworldhome/android/touchme/MainActivity.java
+++ apps/TouchMe/src/com/helloworldhome/android/touchme/MainActivity.java
@@ -57,17 +57,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }
